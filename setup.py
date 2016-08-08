57d14ea1e9b7818ac12a521304662cdddbbc5c3c
Brian Jinwright <bjinwright@qwigo.com>
2016-08-08 15:59:16 -0400

    updated the version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='eva',
-    version='0.1',
+    version='0.1.1',
     description='Plugin for virtualenvwrapper to automatically '
                 'export config vars found in your project level '
                 '.env file.',
